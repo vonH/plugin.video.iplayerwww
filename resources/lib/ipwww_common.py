bf4ef1bda2eb7c9389a970d0b7e2acd1d5a3d2e1
CaptainT <CaptainTK@users.noreply.github.com>
2017-02-04 12:02:57 +0100

    [version] 2.5.4

--- resources/lib/ipwww_common.py
+++ resources/lib/ipwww_common.py
@@ -35,7 +35,7 @@ addonid = "plugin.video.iplayerwww"
 addoninfo = GetAddonInfo()
 DIR_USERDATA = xbmc.translatePath(addoninfo["profile"])
 cookie_jar = None
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:48.0) Gecko/20100101 Firefox/48.0'
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0'
 headers = {'User-Agent': user_agent}
 
 
