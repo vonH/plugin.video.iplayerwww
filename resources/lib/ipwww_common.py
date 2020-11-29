e46e96416e5ab2182ca234df6ef265b511be6988
CaptainT <CaptainTK@users.noreply.github.com>
2020-11-29 08:37:11 +0100

    Fixed one missing Pythin 3 switch.

--- resources/lib/ipwww_common.py
+++ resources/lib/ipwww_common.py
@@ -411,7 +411,10 @@ def utf8_quote_plus(unicode):
 
 # Gets a unicode string from a 'urlencoded' string
 def utf8_unquote_plus(str):
-    return urllib.unquote_plus(str).decode('utf-8')
+    if major_version == 2:
+        return urllib.unquote_plus(unicode.encode('utf-8'))
+    elif major_version == 3:
+        return urllib.parse.unquote_plus(unicode.encode('utf-8'))
 
 
 def AddMenuEntry(name, url, mode, iconimage, description, subtitles_url, aired=None, resolution=None, logged_in=False):
